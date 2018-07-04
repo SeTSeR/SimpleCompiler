cba4e42631cef62e6b02b2d127d76234f161fa9c
Sergey Makarov <SeTSeR200018@gmail.com>
2018-07-04 19:34:27 +0300

    First approach to build solver

---
+++ solver/src/main.c
@@ -0,0 +1,20 @@
+#include "maths.h"
+
+#include <stdio.h>
+
+extern double a, b;
+
+double f1(double);
+double f2(double);
+double f3(double);
+double df1(double);
+double df2(double);
+double df3(double);
+
+function funcs[3] = {&f1, &f2, &f3};
+function derivs[3] = {&df1, &df2, &df3};
+
+int main(int argc, char** argv) {
+    printf("%lf\n", a, b, funcs, derivs);
+    return 0;
+}
---
+++ solver/src/maths.c
@@ -0,0 +1,5 @@
+#include "maths.h"
+
+double area(double a, double b, function funcs[], function derivatives[]) {
+    return 0;
+}
